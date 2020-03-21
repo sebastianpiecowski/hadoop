d2848d27e2a7f03dfbcacb6b6f7db57e8da56306
Sebastian Piecowski <sebastian.piecowski@exaco.pl>
2020-03-21 16:46:22 +0100

    Init config, works properly, build success, port forward success, job executed successfully

---
+++ shells/system/generic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "------- shell generic -------"
+
+cp /vagrant/files/system/bashrc /home/vagrant/.bashrc
+cp /vagrant/files/system/hosts /etc/hosts
+cp /vagrant/files/system/config /home/vagrant/.ssh/config
+
+cat /vagrant/files/hadoop/masters > /etc/dsh/group/cluster
+cat /vagrant/files/hadoop/slaves >> /etc/dsh/group/cluster
+cat /vagrant/files/hadoop/slaves > /etc/dsh/group/slaves
